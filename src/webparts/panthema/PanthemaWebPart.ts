e70cad31e20cb62d5741273e7f767bc22195ecb4
Stefan Bauer <stefan.bauer@n8d.at>
2019-04-17 23:54:37 +0200

    Update to version 1.8.1

--- src/webparts/panthema/PanthemaWebPart.ts
+++ src/webparts/panthema/PanthemaWebPart.ts
@@ -1,7 +1,9 @@
 import { Version } from '@microsoft/sp-core-library';
-
-import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
-import { IPropertyPaneConfiguration, PropertyPaneTextField } from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
 
 import { escape } from '@microsoft/sp-lodash-subset';
 
